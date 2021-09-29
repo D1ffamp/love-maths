807900bea6dee0cfe382218e1f3cfe97464448c6
D1ffamp <marcam82@gmail.com>
2021-09-29 16:55:19 +0000

    added division question

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,9 @@ function runGame(gameType) {
      else if(gameType === "subtract") {
          displaySubtractQuestion(num1,num2);
      }
+      else if(gameType === "division"){
+          displayDivisionQuestion(num1,num2);
+      }
      else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -90,8 +93,9 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if(operator === "-") {
         return[operand1 - operand2, "subtract"];
-    }
-     else {
+    } else if(operator === "/") {
+        return[operand1 / operand2, "division"];
+    } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -137,3 +141,19 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
 }
+
+function displayDivisionQuestion(operand1, operand2){
+    if(operand1>operand2&&operand1&operand2===0){
+        document.getElementById('operand1').textContent = operand1;
+    }else {
+        document.getElementById('operand1').textContent = operand2;
+    }
+    if(operand1>operand2&&operand1&operand2===0){
+        document.getElementById('operand2').textContent = operand2;
+    }else {
+        document.getElementById('operand1').textContent = operand1;
+    }
+    document.getElementById('operator').textContent = "/";
+
+    }
+    
